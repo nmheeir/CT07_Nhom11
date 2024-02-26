0fb701130919f4046d2c548594650ee09a34d98f
nmheir <nmh7624@gmail.com>
2024-02-26 11:51:30 +0700

    gia

--- public/js/showToast.js
+++ public/js/showToast.js
@@ -1,15 +1,22 @@
 function showToast(message) {
     var container = document.querySelector('.container');
     var toast = document.createElement('div');
-    toast.classList.add('toast', 'show');
+    toast.classList.add('toast', 'show', 'w-100');
     toast.innerHTML = `
         <div class="toast-header">
             <strong class="me-auto">Toast Header</strong>
             <button type="button" class="btn-close" data-bs-dismiss="toast"></button>
         </div>
         <div class="toast-body">
-            <p>Some text inside the toast body</p>
+            <p>${message}</p>
         </div>
     `;
     container.appendChild(toast);
+
+    var bootstrapToast = new bootstrap.Toast(toast);
+
+    // Ẩn toast sau 3 giây
+    setTimeout(function() {
+        bootstrapToast.hide();
+    }, 3000);
 }
